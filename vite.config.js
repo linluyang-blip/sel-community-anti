4d521792669349a7ae3dfe98e351d245d0a24558
Lin <linluyang@gmail.com>
2026-04-09 14:45:28 +0800

    feat: separate outcomes into a new page

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   base: '/sel-community-anti/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        outcomes: resolve(__dirname, 'outcomes.html')
+      }
+    }
+  }
 })
